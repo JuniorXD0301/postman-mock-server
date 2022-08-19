9508b0655af0d52cb4c3cc0498a5019485550ab9
Junior Corredor <rsi.dev24@uis.edu.co>
2022-08-19 12:07:32 -0500

    final changes

--- src/testsPostman.js
+++ src/testsPostman.js
@@ -1,6 +1,6 @@
-pm.test("Verify key as_token in response", function(){
-    pm.response.to.have.jsonBody("as_token");
-    pm.environment.unset("url_bearer_token");
-    var json = JSON.parse(responseBody);
+pm.test("Verify key as_token in response", function(){ /*Validar si as token existe*/
+    pm.response.to.have.jsonBody("as_token"); /* Obtener la respuesta*/
+    pm.environment.unset("url_bearer_token"); /* eliminar una varibale de entorno si existe*/
+    var json = JSON.parse(responseBody); /*crear una variable JSON */
     postman.setEnvironmentVariable("url_bearer_token", `Bearer ${json.as_token}`);
 })
